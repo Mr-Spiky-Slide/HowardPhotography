9df7d37f0ddbf39740256ed5dedee93802f8a45c
JackHoward <jhoward000@live.com>
2025-01-18 18:52:11 -0600

    rearrange

--- js/main.js
+++ js/main.js
@@ -15,4 +15,17 @@ $(function(){
     });
 
 
+    //Better kind words sideshow
+    let currentSlide = 0; 
+        const slides = document.querySelectorAll('.kind-words-slide');
+
+        function moveSlide(direction) {
+            slides[currentSlide].style.transform = `translateX(${100 * currentSlide}%)`;
+            currentSlide = (currentSlide + direction + slides.length) % slides.length;
+            slides[currentSlide].style.transform = `translateX(0)`;
+        }
+
+        setInterval(() => moveSlide(1), 5000); // Auto-slide every 5 seconds
+
+
 });
